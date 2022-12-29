3dee4976e837c9c32f43465a3229fb53b56bdefb
Samir Ergaibi <38135462+samirergaibi@users.noreply.github.com>
2022-12-29 09:00:15 +0100

    remove redundant Date.now

--- src/workflow-handler.ts
+++ src/workflow-handler.ts
@@ -59,7 +59,7 @@ export class WorkflowHandler {
   async triggerWorkflow(inputs: any) {
     try {
       const workflowId = await this.getWorkflowId();
-      this.triggerDate = new Date(Date.now()).setMilliseconds(0);
+      this.triggerDate = new Date().setMilliseconds(0);
       const dispatchResp = await this.octokit.actions.createWorkflowDispatch({
         owner: this.owner,
         repo: this.repo,
